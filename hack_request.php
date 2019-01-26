e52d135cc2b7edc6d3a44894f7a202fd6ffcd58d
jktightwad <47039589+jktightwad@users.noreply.github.com>
2019-01-25 22:36:29 -0600

    Update hack_request.php

--- hack_request.php
+++ hack_request.php
@@ -5,14 +5,24 @@
 	SecurityMod::login();
 
 
-	HackMod::addHackRequest(v($_REQUEST, "universe"), v($_REQUEST, "method"), v($_REQUEST, "pilot"), v($_REQUEST, "faction"));
+//	HackMod::addHackRequest(v($_REQUEST, "universe"), v($_REQUEST, "method"), v($_REQUEST, "pilot"), v($_REQUEST, "faction"));
 
-	header(sprintf(
-		"Location: hacks.php?Universe=%s",
+//	HackMod::addHackRequest($universe, $method, $pilot, $faction);	
+	
+/* 	header(sprintf(
+		"Location: hacks.php",
 		$_REQUEST["universe"],
 		$_REQUEST["method"],
 		$_REQUEST["pilot"],
 		$_REQUEST["faction"]
-	));
+	)) */
+	
+	$universe = $_REQUEST["universe"];
+	$method = $_REQUEST["method"];
+	$pilot = $_REQUEST["pilot"];
+	$faction = $_REQUEST["faction"];
+echo "<script type='text/javascript'>alert($universe);</script>";
+//echo $pilot;
+	;
 
 ?>
